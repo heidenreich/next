95e720ebb27f0078fcdb07e75466a3c83b24fdbb
Todd Heidenreich <jtheidenreich@gmail.com>
2014-03-19 15:37:08 -0400

    basic page design and layout

--- app/scripts/temp-light.js
+++ app/scripts/temp-light.js
@@ -13,13 +13,13 @@ board.on("ready", function() {
   // create a new temp sensor instance
   var sensor = new five.Sensor({
     pin: "A0",
-    freq: 5000
+    freq: 1000
   });
 
   // create a new photoresistor hardware instance
   photoresistor = new five.Sensor({
     pin: "A1",
-    freq: 5000,
+    freq: 1000,
 
   });
 
@@ -30,15 +30,14 @@ board.on("ready", function() {
   // "data" get the current reading from the photoresistor
   photoresistor.on("data", function() {
     lightLevel = this.value;
-    // 255,84*this.value
     console.log("the light level is "+ lightLevel);
   });
 
   // "data" get the current reading from the temperature sensor
   sensor.on("data", function() {
     voltage = this.value * 0.004882814;
-    celsius = (voltage - 0.5) * 100;
-    fahrenheit = celsius * (9 / 5) + 32;
+    celsius = ((voltage - 0.5) * 100).toFixed(2);
+    fahrenheit = (celsius * (9 / 5) + 32).toFixed(2);
     console.log(celsius + "°C", fahrenheit + "°F");
 
 
@@ -46,17 +45,17 @@ board.on("ready", function() {
 
 });
 
-setInterval(function() {
-  request.post({
-    url: 'http://tiny-pizza-server.herokuapp.com/collections/weather',
-    form:{
-      lightIntensity: lightLevel,
-      temperature: fahrenheit,
-      location: 'lecture hall',
-      time: Date.now()
-    }
-  });
-}, 10000);
+// setInterval(function() {
+//   request.post({
+//     url: 'http://tiny-pizza-server.herokuapp.com/collections/weather',
+//     form:{
+//       lightIntensity: lightLevel,
+//       temperature: fahrenheit,
+//       location: 'lecture hall',
+//       time: Date.now()
+//     }
+//   });
+// }, 10000);
 
 
 
