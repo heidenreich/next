5253f5fd52f3f0a3530a30b271c5a740a101760c
Todd Heidenreich <jtheidenreich@gmail.com>
2014-03-28 12:46:42 -0400

    adjusted delay

--- app/scripts/temp-light.js
+++ app/scripts/temp-light.js
@@ -10,57 +10,46 @@ fahrenheit = 0
 
 board.on("ready", function() {
 
-  // create a new temp sensor instance
-  var sensor = new five.Sensor({
-    pin: "A0",
-    freq: 1000
-  });
+    // create a new temp sensor instance 
+    var sensor = new five.Sensor({
+        pin: "A0",
+        freq: 1000
+    });
 
-  // create a new photoresistor hardware instance
-  photoresistor = new five.Sensor({
-    pin: "A1",
-    freq: 1000,
+    // create a new photoresistor hardware instance
+    photoresistor = new five.Sensor({
+        pin: "A1",
+        freq: 1000,
+    });
 
-  });
+    board.repl.inject({
+        pot: photoresistor
+    });
 
-  board.repl.inject({
-    pot: photoresistor
-  });
+    // "data" get the current reading from the photoresistor
+    photoresistor.on("data", function() {
+        lightLevel = this.value;
+        console.log("the light level is " + lightLevel);
+    });
 
-  // "data" get the current reading from the photoresistor
-  photoresistor.on("data", function() {
-    lightLevel = this.value;
-    console.log("the light level is "+ lightLevel);
-  });
-
-  // "data" get the current reading from the temperature sensor
-  sensor.on("data", function() {
-    voltage = this.value * 0.004882814;
-    celsius = ((voltage - 0.5) * 100).toFixed(2);
-    fahrenheit = (celsius * (9 / 5) + 32).toFixed(2);
-    console.log(celsius + "°C", fahrenheit + "°F");
-
-
-  });
+    // "data" get the current reading from the temperature sensor
+    sensor.on("data", function() {
+        voltage = this.value * 0.004882814;
+        celsius = ((voltage - 0.5) * 100).toFixed(2);
+        fahrenheit = (celsius * (9 / 5) + 32).toFixed(2);
+        console.log("Todd is chillin at", fahrenheit + "°F");
+    });
 
 });
 
 setInterval(function() {
-  request.post({
-    url: 'http://tiny-pizza-server.herokuapp.com/collections/weather',
-    form:{
-      lightIntensity: lightLevel,
-      temperature: fahrenheit,
-      location: 'lecture hall',
-      time: Date.now()
-    }
-  });
-}, 10000);
-
-
-
-
-
-
-
-
+    request.post({
+        url: 'http://tiny-pizza-server.herokuapp.com/collections/weather',
+        form: {
+            lightIntensity: lightLevel,
+            temperature: fahrenheit,
+            location: 'workspace de Todd',
+            time: Date.now()
+        }
+    });
+}, 60000);
